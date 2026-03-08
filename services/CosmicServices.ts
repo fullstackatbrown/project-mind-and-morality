802e97a70ae6352e8937ed329dad1214a89229ca
Riley <rileyrosenberger23@gmail.com>
2026-03-08 16:37:32 -0400

    get team members adjusted to return list of non_undergrads first and list of undergrads second + interfaces for a couple types added at top of file since no types.ts file yet

---
+++ services/CosmicServices.ts
@@ -0,0 +1,59 @@
+import { cosmic } from "../lib/cosmic";
+
+
+// CLASS TO INTERACT WITH COSMIC BACKEND (for now one file but can split into multiple if gets too big)
+// interface for a TeamMember -- should be moved to a types.ts file, but placed here for now
+interface CosmicMedia{
+  // url for the image/file
+  url: string;
+  // CDN optimized image URL, lets you add query params for resizing, compression, and cropping --- most likely not necessary for us
+  imgix_url?: string;
+  // original filename if needed
+  name?: string;
+}
+
+interface TeamMember {
+  title?: string;
+  slug?: string;
+
+  metadata: {
+    name: string;
+    role: string;
+    description: string;
+    undergraduate: boolean;
+
+    linkedin?: string;
+    website?: string;
+    email?: string;
+    cv?: CosmicMedia;
+
+    profilePhoto?: CosmicMedia;
+  };
+}
+
+class CosmicServices {
+    /**
+     * method to get Team Members
+     * 
+     * @returns two lists of TeamMembers, the first the list of non undergrads (to be displayed at the top of the page) and the second the list of undergrads
+     */
+     
+    getTeamMembers = async (): Promise<[TeamMember[], TeamMember[]]> => {
+        const data = await cosmic.objects.find({ type: "team-members" });
+
+        const raw_objects = data.objects as TeamMember[];
+
+        const non_undergrads = raw_objects.filter(
+            (member) => !member.metadata.undergraduate
+        );
+
+        const undergrads = raw_objects.filter(
+            (member) => member.metadata.undergraduate
+        );
+
+        return [non_undergrads, undergrads];
+    };
+
+}
+
+export default CosmicServices;
