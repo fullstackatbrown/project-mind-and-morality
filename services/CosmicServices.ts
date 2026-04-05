5024d542adf4b5b5af2595561c3b90e0a2d4d41b
Riley <rileyrosenberger23@gmail.com>
2026-04-05 13:40:51 -0400

    types file created

--- services/CosmicServices.ts
+++ services/CosmicServices.ts
@@ -1,76 +1,9 @@
 import { cosmic } from "../lib/cosmic";
+import { TeamMember, NewsPost, NewsPostRaw, NewsPostThumbnail, NewsPostThumbnailRaw } from "./CosmicTypes";
+
 
 
 // CLASS TO INTERACT WITH COSMIC BACKEND (for now one file but can split into multiple if gets too big)
-
-// Types : 
-
-// interface for a TeamMember -- should be moved to a types.ts file, but placed here for now
-interface CosmicMedia{
-  // url for the image/file
-  url: string;
-  // CDN optimized image URL, lets you add query params for resizing, compression, and cropping --- most likely not necessary for us
-  imgix_url?: string;
-  // original filename if needed
-  name?: string;
-}
-
-interface TeamMember {
-  id: string;
-  title?: string;
-  slug?: string;
-  status: string; // published or draft
-
-  metadata: {
-    name: string;
-    role: string;
-    description: string;
-    undergraduate: boolean;
-    displayOrder?: number;
-
-    linkedin?: string;
-    website?: string;
-    email?: string;
-    cv?: CosmicMedia;
-
-    profilePhoto?: CosmicMedia;
-  };
-}
-
-interface NewsPostThumbnailRaw {
-  id: string;
-  title?: string;
-  slug?: string;
-  created_at: string;
-  modified_at: string; 
-
-  metadata: {
-    publish_date : string;
-    featured_image: CosmicMedia;
-    summary: string;
-  };
-}
-
-interface NewsPostRaw extends NewsPostThumbnailRaw {
-  metadata: NewsPostThumbnailRaw['metadata'] & {
-    content: string; // IMPORTANT NOTE : the string returned is a string of HTML content (e.g. "<h1> Title </h1> <p> This is the first paragraph of the article </p>")
-  };
-}
-
-interface NewsPostThumbnail extends Omit<NewsPostThumbnailRaw, "created_at" | "modified_at" | "metadata"> {
-  created_at: Date;
-  modified_at : Date;
-  metadata: Omit<NewsPostThumbnailRaw["metadata"], "publish_date"> & {
-    publish_date : Date;
-  }
-}
-interface NewsPost extends Omit<NewsPostRaw, "created_at" | "modified_at" | "metadata"> {
-  created_at: Date;
-  modified_at: Date;
-  metadata: Omit<NewsPostRaw["metadata"], "publish_date"> & {
-    publish_date : Date;
-  }
-}
 
 class CosmicServices {
     /**
