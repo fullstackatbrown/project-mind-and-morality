76260487196c8da06eda5b9878932d380a92ab1d
Riley <rileyrosenberger23@gmail.com>
2026-04-24 20:38:05 -0400

    another small fix for families page

--- app/get-involved/families/page.tsx
+++ app/get-involved/families/page.tsx
@@ -3,6 +3,7 @@
 import { DM_Sans } from "next/font/google"
 import { useState, useEffect } from "react";
 import FamiliesSlider from "@/app/components/FamiliesSlider";
+import { GetInvolvedFamiliesPage } from "@/services/CosmicTypes";
 
 const dmSans = DM_Sans({
 	subsets: ["latin"],
@@ -11,7 +12,7 @@ const dmSans = DM_Sans({
 
 
 export default function GetInvolvedFamilies() {
-  const [items, setItems] = useState([]);
+  const [pageData, setPageData] = useState<GetInvolvedFamiliesPage | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
@@ -21,7 +22,7 @@ export default function GetInvolvedFamilies() {
         const res = await fetch("/api/get-involved/families");
         if (!res.ok) throw new Error("Failed to fetch");
         const data = await res.json();
-        setItems(data?.metadata?.opportunities || []);
+		setPageData(data);
       } catch (e) {
         setError("Could not load opportunities.");
       } finally {
@@ -36,11 +37,12 @@ export default function GetInvolvedFamilies() {
       <section className="mx-auto w-full max-w-[1180px] px-6 md:px-10">
         <header className="text-center">
           <h1 className="text-[34px] leading-[1.15] font-semibold tracking-[-0.02em] text-[#5b96a0] md:text-[40px]">
-            Get Involved for Families
+			{pageData?.title}
           </h1>
           <p className="mt-7 text-[24px] leading-[1.2] font-semibold tracking-[-0.01em] text-[#cf8c2f] md:text-[30px]">
-            There are many ways to get involved in research!
+			{pageData?.metadata.subheader}
           </p>
+
         </header>
 
         {loading ? (
@@ -48,7 +50,7 @@ export default function GetInvolvedFamilies() {
         ) : error ? (
           <div className="mt-16 text-center text-red-500">{error}</div>
         ) : (
-          <FamiliesSlider items={items} />
+          <FamiliesSlider items={pageData?.metadata?.opportunities || []} />
         )}
       </section>
     </main>
